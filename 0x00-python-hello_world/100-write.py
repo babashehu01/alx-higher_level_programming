33a348722860d04dae675fb8d07d7ca402654402
babashehu01 <shettimababashehu57@gmail.com>
2022-06-02 07:00:21 +0100

    Fixed invalid output

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 import sys
-print("and that piece of art is useful - Dora Korpar, 2015-10-19", file=sys.stderr)
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
+exit(1)
