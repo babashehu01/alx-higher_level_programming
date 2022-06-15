c93e55a4cbb7943be5ec43fda5adb86ffd31b6b1
babashehu01 <shettimababashehu57@gmail.com>
2022-06-15 14:41:46 +0100

    Test commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number != 98:
-        print("{}, ".format(number), end='')
-    else:
-        print("{}".format(number))
+        print(str(number).rjust(2, '0'), end=', ')
