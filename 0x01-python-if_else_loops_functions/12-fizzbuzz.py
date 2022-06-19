2b005d56956370e26673c8aa2585d7d8cb383484
babashehu01 <shettimababashehu57@gmail.com>
2022-06-19 22:13:32 +0100

    Changed to elif

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for number in range(1, 100):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz")
-        else if number % 3 == 0:
+        elif number % 3 == 0:
             print("Fizz ", end='')
-        else if number % 5 == 0:
+        elif number % 5 == 0:
             print("Buzz ", end='')
         else:
             print(number)
