c10cb7e08a4697b2b41f075c343af1b22ea40e75
babashehu01 <shettimababashehu57@gmail.com>
2022-06-19 18:34:52 +0100

    Printed usinf .string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        print(str(number).rjust(2, '0'), end=', ')
+        print("{:02d}, " .format(number), end='')
     else:
-        print(number)
+        print("{:02d}".format(99))
