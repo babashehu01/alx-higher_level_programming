14e6caad32aae3c615cad0f4fd90a9b6e112322e
babashehu01 <shettimababashehu57@gmail.com>
2022-06-19 19:56:08 +0100

    Adding new file

---
+++ 0x02-python-import_modules/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new = ''
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            new += chr(ord(char) - 32)
+        else:
+            new += char
+    print(new)
