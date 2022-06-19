b759ae61c1351141288ee49aa21f19cf39782ae3
babashehu01 <shettimababashehu57@gmail.com>
2022-06-19 18:23:38 +0100

    Fixed newline

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print(str(number).rjust(2, '0'), end=', ')
-print()
+    if number != 99:
+        print(str(number).rjust(2, '0'), end=', ')
+    else:
+        print(number)
