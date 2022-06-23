086565b025df0b1066d7cf44d9eb2a03819da447
babashehu01 <shettimababashehu57@gmail.com>
2022-06-24 00:07:34 +0100

    Adding function

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a class Square"""
+
+
+class Square:
+    """
+    Adding to the class a private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
