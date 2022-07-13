8b05e2d3138e0b061fdc14af0f808a653e6c0a1c
babashehu01 <shettimababashehu57@gmail.com>
2022-07-13 17:39:57 +0100

    Added 0-add files

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == '__main__':
-    from sys import argv
-    numb = 0
-    for index in argv[1:]:
-        numb += int(index)
-    print('{:d}'.format(numb))
--- 0x02-python-import_modules/8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    new = ''
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            new += chr(ord(char) - 32)
-        else:
-            new += char
-    print("{:c}".format(new))
