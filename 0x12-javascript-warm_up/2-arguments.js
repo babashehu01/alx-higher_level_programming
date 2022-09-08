41b5e43d087871d9eaba421007676c4fa23bcca0
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-09-08 10:37:17 +0100

    Fixing requirement fail

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,7 @@ const argV = process.argv.length;
 if (argV == 2) {
 	console.log('No argument');
 }
-else if (argV == 3)
-{
+else if (argV == 3) {
 	console.log('Argument found');
 }
 else if (argV > 3) {
