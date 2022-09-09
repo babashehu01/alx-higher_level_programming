ca8cd9487c2bc65094995785486020706f410ef8
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-09-09 22:45:20 +0100

    Adding a script that prints using array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 // but by using an array of string and a loop
 const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of text) {
-  console.log(text[i]);
+  console.log(i);
 }
