0cfcda6a0dc8b5517430a73a1b0f443c3b6dacf8
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-09-09 17:53:47 +0100

    Fixing the comparison operator

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argV = process.argv.length;
-if (argV == 2) {
+if (argV === 2) {
 	console.log('No argument');
 }
-else if (argV == 3) {
+else if (argV === 3) {
 	console.log('Argument found');
 }
 else if (argV > 3) {
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints to argValue with 'is'
+const argV = process.argv.length
+console.log(argV[3]);
+console.log(' Is ');
+console.log(argV[4]);
