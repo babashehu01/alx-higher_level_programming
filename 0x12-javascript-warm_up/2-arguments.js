7f729276874c427e82f9719be0710ab25923d85c
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-09-09 18:00:46 +0100

    Fixing coding style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const argV = process.argv.length;
 if (argV === 2) {
-	console.log('No argument');
-}
-else if (argV === 3) {
-	console.log('Argument found');
-}
-else if (argV > 3) {
-	console.log('Arguments found');
-}
-else {
-	console.log('No argument found');
+  console.log('No argument');
+} else if (argV === 3) {
+  console.log('Argument found');
+} else (argV > 3) {
+  console.log('Arguments found');
 }
