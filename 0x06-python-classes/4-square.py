bb70dc33f62286d4352857fa8ca473f79f6f289f
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-09-17 21:43:52 +0100

    Refactored the 4-square.py program

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,21 +5,30 @@ Defining a class Square
 
 
 class Square:
-    """___Initializing size___"""
+    """Represents a Square"""
+
     def __init__(self, size=0):
-        self.__size = size
-    """Setting size"""
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square
+        """
+        self.size = size
+
     @property
     def size(self):
-        return self.__size
-    """Getting size"""
+        """Returns the value of size"""
+        return (self.__size)
+    
     @size.setter
     def size(self, value):
+        """Sets the value of size"""
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.value = __size
-    """Public instance method"""
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
     def area(self):
-        return (self.__size ** self.__size)
+        """Public instance method that returns the area"""
+        return (self.__size * self.__size)
