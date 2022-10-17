bafbd1d6262c4f8e23e0b0e7607105e47b31bb3e
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-17 09:09:00 +0100

    Fixing requirement fail

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,10 +3,10 @@
 
 
 class Sqaure:
-    """Intiatializes the class with size"""
+    """Represents a square"""
     def __inti__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be an integer")
+            raise ValueError("size must be >= 0")
         self.__size = size
