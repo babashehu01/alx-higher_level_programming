a51d0ad22da001145ad0df0f523a0865a1e22286
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-17 11:57:48 +0100

    Adding class documentation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines a class Square"""
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square"""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initialize a new square
         Args:
-            size (int): The size of the new square.
+            size (int): Size of the new square
         """
         self.size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """Gets/returns the current size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """sets the value of size"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
+        """Return the current area of our square"""
         return (self.__size * self.__size)
