2589af7925214e2089244231d4751944ae52d35c
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-20 01:49:44 +0100

    Answer

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Defines a LockedClass'''
+
+
+class LockedCkass:
+    '''Represents a class that is Locked'''
+    def __init__(self):
+        '''intializes a locked class'''
+
