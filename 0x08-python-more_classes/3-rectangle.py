72bf2d037ebc29dc5938b1cda8f7d294b1c71983
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-20 16:15:35 +0100

    Fixed pycodestyle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         '''returns the perimeter of the Recctangle'''
         if self.__width == 0 or self.__height == 0:
             return 0
-        return  (2 * (self.__width + self.__height))
+        return (2 * (self.__width + self.__height))
 
     def __str__(self):
         '''repr representation of Rectangle'''
