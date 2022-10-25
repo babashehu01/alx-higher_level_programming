2b57eeec7f5b15169084df23fb304a6170e9e389
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-25 15:49:28 +0100

    A function that writes to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -17,5 +17,5 @@ def write_file(filename="", text=""):
     with open(filename, mode="w", encoding="utf-8") as file:
         file.write(text)
         size = file.tell()
-        
+
     return (size)
