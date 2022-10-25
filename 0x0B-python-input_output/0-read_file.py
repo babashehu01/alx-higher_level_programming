6a8ebb164672336c1d5daf14cc465d0e96d79d46
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-25 15:27:55 +0100

    Adding file reading function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-
+This module defines a file reading unction
 """
 
 
 def read_file(filename=""):
     """
+    This function writes/prints the contents of a file to stdout
 
+    Args:
+        filename: The file to be printed
     """
     with open(filename, encoding="utf-8") as file:
-        file.read()
+        output = file.read()
+
+    print(output, end="")
