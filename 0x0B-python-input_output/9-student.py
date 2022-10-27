ca2b0f0cf2e4047a14195ea060854bdb9a19049f
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-28 00:06:52 +0100

    retrieves the dictionary representation of a Student instance

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a class Student
+"""
+
+
+class Student:
+    """
+    Represents a Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Intsantiates our student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of Student
+        """
+        return (self.__dict__)
