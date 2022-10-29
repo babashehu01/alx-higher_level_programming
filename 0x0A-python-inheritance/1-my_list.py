38bfdaee430fb475ec3832c62947dafea36fced2
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-29 21:32:40 +0100

    Adding a list function

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Deines `MyList` class
+"""
+
+
+class MyList(list):
+    """ 
+    Represents a list
+    """
+    def print_sorted(self):
+        """
+        prints a list in sorted order
+        """
+        print(sorted(self))
