387079529d923b16c1153941dee13fb33516ccaa
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-29 22:50:25 +0100

    Function that checks for instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Defines `is_kind_of_class` function'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Checks is an object is a subclass of class'''
+    return isinstance(obj, a_class)
