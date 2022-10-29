e1f1fe8ec0dd4a3e9343f595502c0876f24f9061
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-29 23:03:25 +0100

    Test commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Defines `inherits_from` function'''
+
+
+def inherits_from(obj, a_class):
+    '''Checks if the obj is a subclass of class'''
+    return isinstance(obj, a_class)
