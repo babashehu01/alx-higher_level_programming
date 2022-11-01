71a7b9d66f8568f2a300dbce7f67d5537a805299
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-11-01 23:19:21 +0100

    Updating Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""
+''' Defines a class Square '''
 
-"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, width, height)
-        self.width = size
-        self.height = size
+        ''' Insatntiates a new Square '''
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        ''' String representation of Square '''
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
