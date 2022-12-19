c60ecdd5ca8ded9de411a5db0c0d3c9def306e7c
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-12-20 00:48:46 +0100

    Printing using loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(text[i]);
 }
