e301ce35250edb93c36cae82eae1f1ee25c4cf6f
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-12-20 02:36:04 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+number = parseInt(process.argv[2])
+let cursed = 1;
+function curse() {
+  if (Number.isNaN(number)) {
+    console.log(1);
+  } else {
+    for (let i = number; i >= 1; i--) {
+      cursed *= i;
+    }
+  console.log(cursed);
+}
+}
+
+curse();
