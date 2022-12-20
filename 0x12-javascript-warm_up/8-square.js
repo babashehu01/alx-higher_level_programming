9514cd6e4f0b068235c681329bd21f236911878d
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-12-20 01:31:34 +0100

    Adding add function

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const squareSize = parseInt(process.argv[2]);
+if (Number.isNaN(squareSize)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < squareSize; i++) {
+    let sqr = '';
+    for(let j = 0; j < squareSize; j++) {
+      sqr += 'X';
+    }
+    console.log(sqr);
+  }
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+a = process.argv[2];
+b = process.argv[3];
+function add(a, b) {
+  console.log(parseInt(a) + parseInt(b));
+}
+
+add(a, b);
