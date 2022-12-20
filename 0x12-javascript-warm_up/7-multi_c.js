289ad23818792a3c01df3dc3cf195a204dfe1fe4
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-12-20 01:03:40 +0100

    Printing C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ if (Number.isNaN(number)) {
   console.log('Missing number of occurrences');
 } else {
   for (i = 0; i < number; i++)
-    {
-      console.log('C is fun');
-    }
+  {
+    console.log('C is fun');
+  }
 }
