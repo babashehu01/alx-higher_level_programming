b65c8eeeb5cf3fabf176032a69874c2ec5d9420a
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-01-21 21:40:34 +0100

    Hot Fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This script fetches a url
-"""
-import urllib.request
+"""script that fetches 'https://alx-intranet.hbtn.io/status' """
+if __name__ == "__main__":
+    import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        page = response.read()
-    print('Body reponse:')
-    print(f'\t- type: {type(page)}')
-    print(f'\t- content: {page}')
-    print(f"\t- utf8 content: {page.decode('UTF8')}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
