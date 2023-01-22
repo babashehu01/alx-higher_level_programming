fa43ad1ccc0986e8f2068e2e4ec9d007774b39c0
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-01-22 23:27:50 +0100

    Posting an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-# POST an email
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
+"""
 import urllib.parse
 import urllib.request
 import sys
 
 url = sys.argv[1]
-values = {'email' : sys.argv[2]}
+values = {'email': sys.argv[2]}
 data = urllib.parse.urlencode(values)
 data = data.encode('ascii')
 req = urllib.request.Request(url, data)
