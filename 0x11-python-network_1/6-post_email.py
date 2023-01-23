123939118eaa5343b03bd648370dab90abaa2ea2
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-01-23 01:45:46 +0100

    Post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script that takes in a URL and an email address,
+sends a POST request to the URL
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {'email': email}
+
+    r = requests.post(url, value)
